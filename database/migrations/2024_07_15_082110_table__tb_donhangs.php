681ef8f5a8ecf5543fc944fdb9480f2454b3b069
Tên của bạn <email@example.com>
2024-07-15 16:08:33 +0700

    Initial commit

--- database/migrations/2024_07_15_082110_table__tb_donhangs.php
+++ database/migrations/2024_07_15_082110_table__tb_donhangs.php
@@ -6,7 +6,8 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
+    /**git push -u origin main
+
      * Run the migrations.
      */
     public function up(): void
@@ -14,7 +15,8 @@ return new class extends Migration
         Schema::create('tb_donhang', function (Blueprint $table) {
             $table->id('id_donhang');
             $table->date('ngay_dat');
-            $table->double('11');
+            $table->double('11');git push -u origin main
+
             $table->boolean('trang_thai');
             $table->boolean('phuong_thuc_tt');
             $table->text('ten_nguoi_mua');
